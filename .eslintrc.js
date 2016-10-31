d0dc13dbf68fdd5f72f4bb46bffca9b17741a7e6
corynorris <corynorris@gmail.com>
2016-10-30 20:29:42 -0400

    Added header parsing for user stories

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,14 @@ module.exports = {
         "react",
         "jsx-a11y",
         "import"
-    ]
+    ],
+    "rules": {
+        // Disable `no-console` rule
+        "no-console": 0,
+        "indent": 4,
+        "arrow-parens": ["error", "always", {
+            "requireForBlockBody": true,
+        }],
+
+    }
 };
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const favicon = require('serve-favicon');
 const express = require('express');
 const path = require('path');
+var platform = require('platform');
 
 const port = process.env.PORT || process.argv[2] || 3000;
 const app = express();
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-
+app
+app.get(['/','/api/whoami'], (req, res) => {
+    const language = req.headers["accept-language"];
+    const ip = req.connection.remoteAddress;
+    res.json({
+        'ipaddress': ip,
+        'language': language,
+        'software': platform.os
+    });
+});
 
 app.listen(port);
